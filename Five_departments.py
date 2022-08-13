0f771d1cf9dde434ff1c97f9ed5d1d8856fa14c9
MalofeevAV <9151532547@mail.ru>
2022-08-13 19:38:03 +0300

    Five_departments

---
+++ Five_departments.py
@@ -0,0 +1,52 @@
+def read_from_file(filename):
+    """Read data from file and return sotred list of applicants"""
+    with open(filename, "r") as file:
+        return create_list_of_applicants(file)
+
+
+def create_list_of_applicants(file):
+    """Split data from file and return list of applicants"""
+    return [applicant.split() for applicant in file]
+
+
+def sorted_list_of_applicants(list_of_applicants):
+    """Sort applicants in ascending order by GPA and alphabetical order by their names and surnames"""
+    return sorted(list_of_applicants, key=lambda x: (-float(x[2]), x[0] + " " + x[1]))
+
+
+def create_dict_with_applicants(list_of_applicants, max_num_of_students):
+    """Create dict with department as a key and inner dict(applicant : GPA) as a value"""
+    dict_with_applicants = dict()
+    enrolled_applicants = []
+
+    # range equals amount of departments
+    for i in range(len(list_of_applicants[0][3:])-1):
+        for applicant in list_of_applicants:
+            department = applicant[3+i]
+            if applicant not in enrolled_applicants:
+                if department not in dict_with_applicants:
+                    dict_with_applicants[department] = {" ".join(applicant[:2]): float(applicant[2])}
+                    enrolled_applicants.append(applicant)
+                else:
+                    # For each department, choose the N best candidates
+                    if len(dict_with_applicants[department]) < max_num_of_students:
+                        dict_with_applicants[department].update({" ".join(applicant[:2]) : float(applicant[2])})
+                        enrolled_applicants.append(applicant)
+
+    return dict_with_applicants
+
+
+def print_result(dict_with_applicants):
+    """Print result(applicant, gpa) sorted by alphabetical order"""
+    for department in sorted(dict_with_applicants):
+        print(department)
+        for applicant, gpa in dict_with_applicants[department].items():
+            print(applicant, gpa)
+        print()
+
+
+if __name__ == "__main__":
+    max_num_of_students = int(input())
+    list_of_applicants = read_from_file("Test_files/test1")
+    dict_with_applicants = create_dict_with_applicants(sorted_list_of_applicants(list_of_applicants), max_num_of_students)
+    print_result(dict_with_applicants)
