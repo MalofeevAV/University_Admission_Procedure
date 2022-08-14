3c8bf1e0ce7cb83879189a4b62b69bc63ad28352
MalofeevAV <9151532547@mail.ru>
2022-08-14 18:07:00 +0300

    Five_deparments(make corrections)

--- Five_departments.py
+++ Five_departments.py
@@ -10,8 +10,9 @@ def create_list_of_applicants(file):
 
 
 def sorted_list_of_applicants(list_of_applicants):
-    """Sort applicants in ascending order by GPA and alphabetical order by their names and surnames"""
-    return sorted(list_of_applicants, key=lambda x: (-float(x[2]), x[0] + " " + x[1]))
+    """Sort applicants in ascending order by GPA and priorities.
+    If two applicants to the same department have the same GPA, it will be sorted by their full names in alphabetical order"""
+    return sorted(list_of_applicants, key=lambda x: (-float(x[2]), x[3], x[0]))
 
 
 def create_dict_with_applicants(list_of_applicants, max_num_of_students):
@@ -20,7 +21,7 @@ def create_dict_with_applicants(list_of_applicants, max_num_of_students):
     enrolled_applicants = []
 
     # range equals amount of departments
-    for i in range(len(list_of_applicants[0][3:])-1):
+    for i in range(len(list_of_applicants[0][3:])):
         for applicant in list_of_applicants:
             department = applicant[3+i]
             if applicant not in enrolled_applicants:
@@ -40,13 +41,14 @@ def print_result(dict_with_applicants):
     """Print result(applicant, gpa) sorted by alphabetical order"""
     for department in sorted(dict_with_applicants):
         print(department)
-        for applicant, gpa in dict_with_applicants[department].items():
-            print(applicant, gpa)
+        for applicant in sorted(dict_with_applicants[department], key=dict_with_applicants[department].get, reverse=True):
+            print(applicant, dict_with_applicants[department][applicant])
         print()
 
 
 if __name__ == "__main__":
     max_num_of_students = int(input())
-    list_of_applicants = read_from_file("Test_files/test1")
+    list_of_applicants = read_from_file("Test_files/applicants.txt")
+    # list_of_applicants = read_from_file("Test_files/test1")
     dict_with_applicants = create_dict_with_applicants(sorted_list_of_applicants(list_of_applicants), max_num_of_students)
     print_result(dict_with_applicants)
